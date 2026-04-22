16ca0047375b75f4fa36fecf478734cf4c9ba5b6
Abdelrahim Adam <133844991+AbdelrahimAdam@users.noreply.github.com>
2026-04-22 20:52:40 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -21,11 +21,11 @@ export default defineConfig({
         orientation: 'portrait',
         icons: [
           { src: '/icons/72.png', sizes: '72x72', type: 'image/png', purpose: 'any maskable' },
-          { src: '/icons/128.png', sizes: '128x128', type: 'image/png' },
-          { src: '/icons/144.png', sizes: '144x144', type: 'image/png' },
-          { src: '/icons/152.png', sizes: '152x152', type: 'image/png' },
-          { src: '/icons/192.png', sizes: '192x192', type: 'image/png' },
-          { src: '/icons/512.png', sizes: '512x512', type: 'image/png' }
+          { src: '/icons/128.png', sizes: '128x128', type: 'image/png', purpose: 'any maskable' },
+          { src: '/icons/144.png', sizes: '144x144', type: 'image/png', purpose: 'any maskable' },
+          { src: '/icons/152.png', sizes: '152x152', type: 'image/png', purpose: 'any maskable' },
+          { src: '/icons/192.png', sizes: '192x192', type: 'image/png', purpose: 'any maskable' },
+          { src: '/icons/512.png', sizes: '512x512', type: 'image/png', purpose: 'any maskable' }
         ]
       },
       workbox: {
